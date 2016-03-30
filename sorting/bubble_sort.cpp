14d91f790d8feb62a07a2df279c56eeee9e7e250
aakash-cr7 <aakash.tyg93@gmail.com>
2016-03-30 22:38:09 +0530

    Improved bubble sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -29,6 +29,23 @@ void bubbleSort(int *a, int len) {
     }
 }
 
+/* Improved version of bubble sort, if array is sorted already then 
+ * time complexity is O(n) i.e. in best case whereas in the 
+ * above function best case will be O(n^2) 
+ */
+void bubbleSortImproved(int *a, int len) {
+    for(int i = 0; i < len - 1; i++) {
+        flag = 1;
+        // Last i elements will already be sorted
+        for(int j = 0; j < len - i - 1; j++) {
+            if(a[j] > a[j + 1]) {
+                swap(&a[j], &a[j + 1]);
+                flag = 1;
+            }
+        }
+        if (flag == 0) break;
+    }
+}
 int main() {
     int a[10] = {5, 3, 1, 4, 6, 8, 2, 9, 7, 10};
     int len = 10;
