5c7526b6de3165b63345896abc04041a4aa410c3
aakash-cr7 <aakash.tyg93@gmail.com>
2016-04-02 22:02:06 +0530

    searching folder created + linear search added

---
+++ searching/linear_search.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int *a, int length, int item_to_find) {
+    for(int i = 0; i < length; i++) {
+        if(a[i] == item_to_find) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int a[7] = {1, 4, 5, 9, 1, 0};
+    int length = 7;
+    int item_to_find = 11;
+    cout << linearSearch(a, length, item_to_find) << endl;
+}
